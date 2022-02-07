6bc2893b2760d9c33df1233748c4a2d923290625
zucong <sebastiancong@163.com>
2022-02-07 17:33:38 +0800

    support compare float with integer

--- template/funcs.go
+++ template/funcs.go
@@ -323,6 +323,10 @@ func eq(arg1 interface{}, arg2 ...interface{}) (bool, error) {
 				truth = v1.Int() >= 0 && uint64(v1.Int()) == v2.Uint()
 			case k1 == uintKind && k2 == intKind:
 				truth = v2.Int() >= 0 && v1.Uint() == uint64(v2.Int())
+			case k1 == intKind && k2 == floatKind:
+				truth = float64(v1.Int()) == v2.Float()
+			case k1 == floatKind && k2 == intKind:
+				truth = v1.Float() == float64(v2.Int())
 			default:
 				return false, errBadComparison
 			}
